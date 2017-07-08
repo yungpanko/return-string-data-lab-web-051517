22bae8d7a3328ed90805c5cdf4cbc23e26e858e4
yungpanko <jrjohns@mit.edu>
2017-07-08 10:45:25 -0400

    Done.

---
+++ db/migrate/20170707181132_add_description_and_inventory_to_product.rb
@@ -0,0 +1,8 @@
+class AddDescriptionAndInventoryToProduct < ActiveRecord::Migration
+  def change
+    change_table :products do |t|
+      t.string :description
+      t.integer :inventory
+    end
+  end
+end
